b75438f970cbbf804ff4ae71ce3d1d3df01d608b
Matt-Seath <100132940+Matt-Seath@users.noreply.github.com>
2022-06-16 15:42:40 +1000

    added tests

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,6 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for: {}", config.query);
-    println!("In file: {}", config.filename);
-
     if let Err(e) = kwikgrep::run(config) {
         println!("Application error: {}", e);
 
