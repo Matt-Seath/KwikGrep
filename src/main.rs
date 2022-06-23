0b9ee801accf45c668f345034f84e9625e98a48f
Matt-Seath <100132940+Matt-Seath@users.noreply.github.com>
2022-06-23 22:15:30 +1000

    refactored code to use iterators

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,8 @@ use std::process;
 use kwikgrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {}", err);
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguements: {}", err);
         process::exit(1);
     });
 
